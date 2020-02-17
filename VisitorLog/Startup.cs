3502a7b0985d67864662c7d89bd7c62c6647c81d
Nicholas Custer <nicholas_custer@ahm.honda.com>
2020-02-17 18:39:41 -0500

    Added production databas connection information.

--- VisitorLog/Startup.cs
+++ VisitorLog/Startup.cs
@@ -45,7 +45,7 @@ namespace VisitorLog
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
